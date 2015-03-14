ecb7db752bad50e103f98fdf90e4bbd28393bff0
Lucas Macdonald <lucas.macd@gmail.com>
2015-03-13 23:32:55 -0400

    Added join table and :through relationship

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,10 +1,11 @@
 class Article < ActiveRecord::Base
-	validates_presence_of :title, :body
+  validates_presence_of :title, :body
 
-    belongs_to :user 
+  belongs_to :user 
+  has_and_belongs_to_many :categories 
+  has_many :comments
 
-	def long_title
-		"#{title} - #{published_at}"
-	end
-	
+  def long_title
+    "#{title} - #{published_at}"
+  end
 end
