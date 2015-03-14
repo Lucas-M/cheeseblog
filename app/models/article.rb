3d76f820bffde8b5d5baec95b4dd9c8831040be9
Lucas Macdonald <lucas.macd@gmail.com>
2015-03-14 11:27:58 -0400

    Added validation to user model...and encryption of password

--- app/models/article.rb
+++ app/models/article.rb
@@ -7,8 +7,14 @@ class Article < ActiveRecord::Base
 
   scope :published, lambda { where("articles.published_at IS NOT NULL") }
   scope :draft, lambda { where("articles.published_at IS NULL") }
+  scope :recent, lambda { published.where("articles.published_at > ?", 1.week.ago.to_date) }
+  scope :where_title, lambda { published.where("articles.published_at > ?", 1.week.ago.to_date) }
 
   def long_title
     "#{title} - #{published_at}"
+  end
+
+  def published?
+    published_at.present?
   end
 end
