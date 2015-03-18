7d4f9cb851b31b4ec3b2e6ebba004b79d5ab8f6f
Lucas Macdonald <lucas.macd@gmail.com>
2015-03-18 16:04:42 -0400

    Typo fix in comment controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -22,6 +22,6 @@ class CommentsController < ApplicationController
   end
 
   def comment_params
-    params.require(:comment.permit(:name, :email, :body)
+    params.require(:comment).permit(:name, :email, :body)
   end
 end
